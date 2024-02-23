8fa3bc1a1d010ffdf3b2d244dda9964ff7c5f6f8
jslotnick <160745628+jslotnick@users.noreply.github.com>
2024-02-23 08:38:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,8 +45,10 @@ try:
    else:
       back_from_function = get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_from_function)
+      
 except URLError as e:
    streamlit.error()
+
 #streamlit.write('The user entered ', fruit_choice)
 
 #streamlit.text(fruityvice_response.json())
