cdaac6fa73b179ef87f046a13c1db17b46f27e97
jslotnick <160745628+jslotnick@users.noreply.github.com>
2024-02-23 11:27:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,13 +9,13 @@ import streamlit
 #my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.title("My Mom's new Healthy Diner")
-#streamlit.header('Breakfast Menu')
-#streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-#streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-#streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-#streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
    
-#streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 # adding some default selections to the selector
