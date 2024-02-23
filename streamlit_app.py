ec9d47cfe2af04b8b325cbb1a77ef173c2527d66
jslotnick <160745628+jslotnick@users.noreply.github.com>
2024-02-23 12:03:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,8 +31,7 @@ streamlit.text(fruityvice_response.json())
 # take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it the screen as a table
-streamlit.dataframe(fruitvice_normalized)
-
+streamlit.dataframe(fruityvice_normalized)
 #only show the selected fruits
 
 
