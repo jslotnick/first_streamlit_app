005b766fb9d3689f1c1105587a9c1ad8adabba8e
jslotnick <160745628+jslotnick@users.noreply.github.com>
2024-02-23 11:26:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-#import streamlit
+import streamlit
 #import pandas
 #import requests
 #import snowflake.connector
@@ -8,7 +8,7 @@
 #my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 #my_fruit_list = my_fruit_list.set_index('Fruit')
 
-#streamlit.title("My Mom's new Healthy Diner")
+streamlit.title("My Mom's new Healthy Diner")
 #streamlit.header('Breakfast Menu')
 #streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 #streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
